cee01fea388513a7694a4818a8bdff1255a98926
YanShenChun <shanghaitan1987@126.com>
2016-09-02 17:42:02 +0800

    add a project level ycm_extra_conf

--- samples/test-mark-stack.cc
+++ samples/test-mark-stack.cc
@@ -12,7 +12,7 @@ using namespace v8::internal;
 
 void TestMarkingStack() {
   // if not comment this, it will cause gdb 'segment fault' issue on src/thirdparty/dtoa/dtoa.c
-  //std::cout << "TestMarkingStack::start" << std::endl;
+  std::cout << "TestMarkingStack::start" << std::endl;
   int mem_size = 20 * kPointerSize;
   byte* mem = NewArray<byte>(20*kPointerSize);
   Address low = reinterpret_cast<Address>(mem);
@@ -35,7 +35,7 @@ void TestMarkingStack() {
   CHECK_EQ(NULL, address);
   DeleteArray(mem);
 
-  //std::cout << "TestMarkingStack::end" << std::endl;
+  std::cout << "TestMarkingStack::end" << std::endl;
 }
 
 int main(int argc, char* argv[]) {
